b179f1a4182e5b55dccb6ee484d41aa18f4a3672
Nico Zimmermann <mail@nico-zimmermann.com>
2016-11-11 20:58:00 +0100

    preferences

--- src/main/java/com/zimmerbell/repaper/Repaper.java
+++ src/main/java/com/zimmerbell/repaper/Repaper.java
@@ -2,6 +2,8 @@ package com.zimmerbell.repaper;
 
 import java.awt.Desktop;
 import java.awt.Graphics2D;
+import java.awt.HeadlessException;
+import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
@@ -13,10 +15,14 @@ import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 import java.awt.image.RescaleOp;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
@@ -39,33 +45,40 @@ import com.sun.jna.platform.win32.WinDef.UINT_PTR;
 import com.sun.jna.win32.StdCallLibrary;
 import com.sun.jna.win32.W32APIFunctionMapper;
 import com.sun.jna.win32.W32APITypeMapper;
+import com.zimmerbell.repaper.sources.MomentumSource;
 import com.zimmerbell.repaper.sources.MuzeiSource;
 
 public class Repaper {
+	private final static Logger LOG = LoggerFactory.getLogger(Repaper.class);
 
 	public final static String HOME = System.getProperty("user.home") + File.separator + ".repaper";
 	public final static int GAUSS_RADIUS = 15;
-	public final static float BRIGTHNESS  = 0.5f;
-	
-	
-	public final static File CURRENT_FILE = new File(HOME, "current.jpg");
-	public final static File CURRENT_FILE_ORIGINAL = new File(HOME, "current-original.jpg");
-	
-	private static final Logger LOG = LoggerFactory.getLogger(Repaper.class);
-	
+	public final static float BRIGTHNESS = 0.5f;
+
+	private final static File CURRENT_FILE = new File(HOME, "current.jpg");
+	private final static File CURRENT_FILE_ORIGINAL = new File(HOME, "current-original.jpg");
+	private final static File CONFIG_FILE = new File(HOME, "repaper.conf");
+	private final static String CONFIG_SOURCE = "source";
+
+	private static enum SourceType {
+		Muzei, Momentum
+	}
+
 	private static Repaper repaperInstance;
 
 	private TrayIcon trayIcon;
+	private Properties config;
+
 	private Source source;
 	private Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 	private JobDetail updateJob = JobBuilder.newJob(UpdateJob.class).build();
 
 	public static void main(String[] args) throws Exception {
-		repaperInstance = new Repaper(new MuzeiSource());
-//		repaperInstance = new Repaper(new MomentumSource());
+		// repaperInstance = new Repaper(new MuzeiSource());
+		repaperInstance = new Repaper(new MomentumSource());
 	}
-	
-	public static Repaper getInstance(){
+
+	public static Repaper getInstance() {
 		return repaperInstance;
 	}
 
@@ -74,30 +87,56 @@ public class Repaper {
 
 		initTray();
 		initScheduler();
+		initConfig();
+	}
+
+	private void initConfig() throws IOException {
+		config = new Properties();
+		try {
+			config.load(new FileInputStream(CONFIG_FILE));
+		} catch (FileNotFoundException e) {
+		}
+
+		switch (SourceType.valueOf(config.getProperty(CONFIG_SOURCE, SourceType.Momentum.name()))) {
+		case Muzei:
+			source = new MuzeiSource();
+			break;
+		case Momentum:
+			source = new MomentumSource();
+			break;
+		}
+		
+		update();
 	}
 
 	private void initScheduler() {
 		Trigger trigger = TriggerBuilder.newTrigger() //
 				.startNow() //
-				.withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(5, 0).withMisfireHandlingInstructionFireAndProceed()) //
+				.withSchedule(
+						CronScheduleBuilder.dailyAtHourAndMinute(5, 0).withMisfireHandlingInstructionFireAndProceed()) //
 				.build();
-		
-		
+
 		try {
 			scheduler.start();
 			scheduler.scheduleJob(updateJob, trigger);
-			
-			// trigger now
-			scheduler.triggerJob(updateJob.getKey());
 		} catch (SchedulerException e) {
 			logError(e);
 		}
 	}
-	
 
 	private void initTray() throws Exception {
 		PopupMenu popup = new PopupMenu();
 		MenuItem mi;
+
+		Menu preferences = new Menu("Preferences");
+		popup.add(preferences);
+
+		Menu sourceMenu = new Menu("Source");
+		preferences.add(sourceMenu);
+
+		for (SourceType sourceType : SourceType.values()) {
+			sourceMenu.add(new ConfigMenuItem(sourceType.name(), CONFIG_SOURCE, sourceType));
+		}
 
 		popup.add(mi = new MenuItem("Update"));
 		mi.addActionListener(new ActionListener() {
@@ -109,15 +148,15 @@ public class Repaper {
 				}
 			}
 		});
-		
+
 		popup.add(mi = new MenuItem("Show original"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				showOriginal();
 			}
 		});
-		
-		if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)){
+
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
 			popup.add(mi = new MenuItem("Details"));
 			mi.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
@@ -127,9 +166,9 @@ public class Repaper {
 						logError(e);
 					}
 				}
-			});	
+			});
 		}
-		
+
 		popup.addSeparator();
 
 		popup.add(mi = new MenuItem("Exit"));
@@ -141,73 +180,88 @@ public class Repaper {
 
 		trayIcon = new TrayIcon(ImageIO.read(Repaper.class.getResourceAsStream("/icon_16.png")), null, popup);
 		trayIcon.setImageAutoSize(true);
-		
+
 		trayIcon.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				showOriginal();
 			}
 		});
-		
+
 		SystemTray.getSystemTray().add(trayIcon);
 	}
-	
-	public void update(){
+
+	private void setConfig(String key, String value) {
+		config.setProperty(key, value);
+		CONFIG_FILE.getParentFile().mkdirs();
+		try {
+			config.store(new FileOutputStream(CONFIG_FILE), "");
+
+			initConfig();
+
+		} catch (Exception e) {
+			logError(e);
+		}
+	}
+
+	public void update() {
 		try {
 			LOG.info("update");
-			
+
 			source.update();
-			
+
 			BufferedImage image = ImageIO.read(new URL(source.getImageUri()).openStream());
-			
-			try{
+
+			try {
 				CURRENT_FILE_ORIGINAL.getParentFile().mkdir();
 				ImageIO.write(image, "jpg", CURRENT_FILE_ORIGINAL);
-	
-				image = blur(image);
-				image = darken(image);
-				
+
+				// image = blur(image);
+				// image = darken(image);
+
 				CURRENT_FILE.getParentFile().mkdirs();
 				ImageIO.write(image, "jpg", CURRENT_FILE);
-				
+
 				trayIcon.setToolTip("\"" + source.getTitle() + "\"\n by " + source.getBy());
-				
+
 				setBackgroundImage(false);
 
 			} catch (Exception e) {
 				trayIcon.setToolTip(null);
 				logError(e);
-			}	
+			}
 		} catch (Exception e) {
 			logError(e);
 		}
 	}
-	
-	private void showOriginal(){
+
+	private void showOriginal() {
 		try {
 			JobKey jobKey = JobKey.jobKey("showJob");
-			//scheduler.interrupt(jobKey);
-			scheduler.scheduleJob(JobBuilder.newJob(ShowJob.class).withIdentity(jobKey).build(), TriggerBuilder.newTrigger().build());
-		}catch(ObjectAlreadyExistsException e){
-			 // do nothing
+			// scheduler.interrupt(jobKey);
+			scheduler.scheduleJob(JobBuilder.newJob(ShowJob.class).withIdentity(jobKey).build(),
+					TriggerBuilder.newTrigger().build());
+		} catch (ObjectAlreadyExistsException e) {
+			// do nothing
 		} catch (SchedulerException e) {
 			logError(e);
 		}
 	}
-	
-	public void setBackgroundImage(boolean original) throws IOException{
+
+	public void setBackgroundImage(boolean original) throws IOException {
 		LOG.info("show " + (original ? " original" : "") + " image");
-		
-		File file = original ? CURRENT_FILE_ORIGINAL : CURRENT_FILE; 
-		
+
+		File file = original ? CURRENT_FILE_ORIGINAL : CURRENT_FILE;
+
 		/**
 		 * source: https://stackoverflow.com/a/4750765/3529762
 		 */
-		SPI.INSTANCE.SystemParametersInfo(new UINT_PTR(SPI.SPI_SETDESKWALLPAPER), new UINT_PTR(0), file.getCanonicalPath(), new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
-		
+		SPI.INSTANCE.SystemParametersInfo(new UINT_PTR(SPI.SPI_SETDESKWALLPAPER), new UINT_PTR(0),
+				file.getCanonicalPath(), new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
+
 	}
-	
+
 	private void exit() {
 		try {
 			scheduler.shutdown();
@@ -216,26 +270,27 @@ public class Repaper {
 		}
 		System.exit(0);
 	}
-	
 
-	public static void logError(Throwable e){
+	public static void logError(Throwable e) {
 		LOG.error(e.getMessage(), e);
 		JOptionPane.showMessageDialog(null, e.getMessage());
 	}
 
 	private BufferedImage blur(BufferedImage image) {
 
-//		 image = gauss(GAUSS_RADIUS).filter(image, null);
+		// image = gauss(GAUSS_RADIUS).filter(image, null);
 
 		image = getGaussianBlurFilter(Repaper.GAUSS_RADIUS, true).filter(image, null);
 		image = getGaussianBlurFilter(Repaper.GAUSS_RADIUS, false).filter(image, null);
 
 		// cropping black borders
-		BufferedImage croppedImage = new BufferedImage(image.getWidth() - (2 * Repaper.GAUSS_RADIUS), image.getHeight() - (2 * Repaper.GAUSS_RADIUS), BufferedImage.TYPE_INT_RGB);
+		BufferedImage croppedImage = new BufferedImage(image.getWidth() - (2 * Repaper.GAUSS_RADIUS),
+				image.getHeight() - (2 * Repaper.GAUSS_RADIUS), BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = croppedImage.createGraphics();
 		g.drawImage(image, //
 				0, 0, croppedImage.getWidth(), croppedImage.getHeight(), //
-				Repaper.GAUSS_RADIUS, Repaper.GAUSS_RADIUS, croppedImage.getWidth() + Repaper.GAUSS_RADIUS, croppedImage.getHeight() + Repaper.GAUSS_RADIUS, //
+				Repaper.GAUSS_RADIUS, Repaper.GAUSS_RADIUS, croppedImage.getWidth() + Repaper.GAUSS_RADIUS,
+				croppedImage.getHeight() + Repaper.GAUSS_RADIUS, //
 				null);
 		image = croppedImage;
 
@@ -256,7 +311,8 @@ public class Repaper {
 		float[][] matrix = new float[(2 * radius) + 1][(2 * radius) + 1];
 		for (int x = 0; x <= radius; x++) {
 			for (int y = 0; y <= radius; y++) {
-				float d = (float) (1 / (2 * Math.PI * sigma * sigma) * Math.exp(-((x * x) + (y * y)) / (2 * sigma * sigma)));
+				float d = (float) (1 / (2 * Math.PI * sigma * sigma)
+						* Math.exp(-((x * x) + (y * y)) / (2 * sigma * sigma)));
 
 				matrix[radius + x][radius + y] = d;
 				matrix[radius + x][radius - y] = d;
@@ -328,6 +384,22 @@ public class Repaper {
 
 		// return new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
 		return new ConvolveOp(kernel);
+	}
+
+	private class ConfigMenuItem extends MenuItem {
+		private static final long serialVersionUID = 1L;
+
+		public ConfigMenuItem(String label, final String key, final Object value) {
+			super(label);
+			
+			LOG.info(key + "=" + value);
+
+			addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					setConfig(key, value.toString());
+				}
+			});
+		}		
 	}
 
 	/**
